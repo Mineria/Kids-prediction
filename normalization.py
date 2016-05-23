e41c116c69ecdae9b25bcb6d7248ddf1acd0162b
Jorge Ferreiro <me@jgferreiro.com>
2016-05-23 11:11:10 +0200

    Merge conflict on normalization

--- normalization.py
+++ normalization.py
@@ -1,33 +1,17 @@
 # --*-- coding:utf-8 --*--
 
-<<<<<<< HEAD
-def normal_time(time):
-    if time < 10:
-=======
 def normalize_operator(operator):
     if operator == "/":
-        return 4 #  even more complexity
+        return 4 #  even more complexity
     elif operator == "*":
-        return 3 #  more complexity
+        return 3 #  more complexity
     elif operator == "-":
->>>>>>> RandomForest
         return 2
-    if time < 20:
+    elif operator == "+":
         return 1
     else:
         return 0
 
-<<<<<<< HEAD
-def normal_operator(operator):
-    if operator == "+":
-        return 1
-    elif operator == "-":
-        return 2
-    elif operator == "/":
-        return 3
-    elif operator == "*":
-        return 4
-=======
 def normalize_time(time):
     if time < 20:
         return 1
@@ -36,18 +20,17 @@ def normalize_time(time):
 
 def server_operation_conversion(operator):
     if operator == 3:
-        return 8  #Division |  even more complexity
+        return 8  #Division |  even more complexity
     elif operator == 2:
-        return 7  #Multiplication | more complexity
+        return 7  #Multiplication | more complexity
     elif operator == 1:
         return 2  #Resta
     elif operator == 0:
         return 1  #Sum
->>>>>>> RandomForest
     else:
-        return 4
+        return 0
 
-def normal_operands(operand):
+def normalize_operands(operand):
     if operand <= 10:
         return 1
     elif operand >= 10 and operand < 20:
